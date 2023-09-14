bb63d97b0f3e64be81111b7da2d16e9c122ab05e
Tanxing1024 <142484848+Tanxing1024@users.noreply.github.com>
2023-09-14 17:19:18 +0800

    Update InventoryService.java
    获取对方库存

--- src/main/java/com/stupidzhe/api/event/inventory/InventoryService.java
+++ src/main/java/com/stupidzhe/api/event/inventory/InventoryService.java
@@ -12,4 +12,12 @@ public interface InventoryService {
      * 获取库存
      */
     String getInventory(Bot bot, String apiId);
+
+    /**
+     * 获取对方库存
+     * @param bot
+     * @param apiId
+     * @return
+     */
+    String getInventoryOthers(Bot bot, String apiId, String referer);
 }
