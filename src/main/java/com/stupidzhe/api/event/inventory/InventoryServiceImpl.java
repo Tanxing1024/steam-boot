a72e25ac9a64bb118175e217628d9a527b0c8fbe
Tanxing1024 <142484848+Tanxing1024@users.noreply.github.com>
2023-09-14 17:19:48 +0800

    Update InventoryServiceImpl.java
    获取对方库存

--- src/main/java/com/stupidzhe/api/event/inventory/InventoryServiceImpl.java
+++ src/main/java/com/stupidzhe/api/event/inventory/InventoryServiceImpl.java
@@ -28,4 +28,38 @@ public class InventoryServiceImpl implements InventoryService {
         }
         return res.getResponse();
     }
+
+    @Override
+    public String getInventoryOthers(Bot bot, String apiId, String referer) {
+        Http http = bot.getHttp();
+        String steamId = getSteamId(bot, referer);
+        String url = "https://steamcommunity.com/tradeoffer/new/partnerinventory/?sessionid=" + bot.getSessionId() + "&partner=" + steamId + "&appid=" + apiId + "&contextid=2";
+        HttpBean res = http.request(url, "GET", null, bot.getCookies().toString(), true, referer, false);
+        int code = res.getCode();
+        if (200 != code) {
+            log.error("请求库存失败:code=" + code);
+            return null;
+        }
+        return res.getResponse();
+    }
+
+    private String getSteamId(Bot bot, String url) {
+        Http http = bot.getHttp();
+        HttpBean res = http.request(url, "GET", null, bot.getCookies().toString(), true, null, false);
+        int code = res.getCode();
+        if (429 == code) {
+            log.error(bot.getApiId() + ":" + bot.getBotNum() + " 访问过于频繁");
+            return null;
+        }
+        String content = res.getResponse();
+        if (null == content) {
+            return null;
+        }
+        Document doc = Jsoup.parse(content);
+        String html = doc.html();
+        int steamIDIndexOf = html.indexOf("g_ulTradePartnerSteamID");
+        int indexOf = html.indexOf("'", steamIDIndexOf);
+        int indexOf1 = html.indexOf("'", indexOf + 1);
+        return html.substring(indexOf + 1, indexOf1);
+    }
 }
